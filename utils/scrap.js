912a7cd809a0f3e1b630db546725c28d16ddefbd
Unknown-Dev7 <unknowndev896@gmail.com>
2025-07-30 22:20:39 +0700

    update utils/scrap.js

---
+++ utils/scrap.js
@@ -0,0 +1,12 @@
+// utils/scrap.js
+const Axios = require("axios");
+const cheerio = require("cheerio");
+const qs = require("qs");
+const FormData = require("form-data");
+
+// Pindahin semua fungsi downloader ke sini
+async function ssstik(url) {
+   // isi kode yang lo udah punya
+}
+
+module.exports = { ssstik }; // export agar bisa dipakai di backend
