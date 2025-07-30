cf189abda67a2975e9ad3b4e80a0f4b5f60b3852
Unknown-Dev7 <unknowndev896@gmail.com>
2025-07-30 22:55:25 +0700

    update api/ssstik.js

--- api/ssstik.js
+++ api/ssstik.js
@@ -4,6 +4,7 @@ import qs from 'qs'
 
 export default async function handler(req, res) {
   const { url } = req.query
+  
   if (!url) return res.status(400).json({ status: false, message: 'Masukkan URL' })
 
   try {
